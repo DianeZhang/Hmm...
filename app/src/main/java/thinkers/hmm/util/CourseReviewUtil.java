a0d6bcbe8fe88740898a857b66896cfcd5bc07de
yao zhou <yaozhou@andrew.cmu.edu>
2015-07-18 17:27:32 -0400

    finish diane's util class

---
+++ app/src/main/java/thinkers/hmm/util/CourseReviewUtil.java
@@ -0,0 +1,32 @@
+package thinkers.hmm.util;
+
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+import thinkers.hmm.model.CourseReview;
+
+/**
+ * Created by Yao on 7/18/15.
+ */
+public class CourseReviewUtil {
+    public boolean insertCourseReview(CourseReview) {
+
+    }
+
+    public ArrayList<CourseReview> selectCourseReview(int ) {
+
+    }
+
+    public ArrayAdapter<CourseReview> selectCourseReview(String) {
+
+    }
+
+    public boolean updateCourseReview(int,CourseReview) {
+
+    }
+
+    public boolean deleteCourseReview(int) {
+
+    }
+}
---
+++ app/src/main/java/thinkers/hmm/util/DatabaseConnectorBase.java
@@ -0,0 +1,14 @@
+package thinkers.hmm.util;
+
+import java.sql.Connection;
+
+/**
+ * Created by Yao on 7/18/15.
+ */
+public abstract class DatabaseConnectorBase {
+    Connection connection;
+
+    abstract public void open();
+    abstract public void close();
+
+}
