2c4e3a4c18985e8a8fda48be033c57bbd49ffe3c
Charlie <chaoli1@andrew.cmu.edu>
2015-07-18 17:22:43 -0400

    add classes in util

---
+++ app/src/main/java/thinkers/hmm/util/CourseFacultyRelationshipUtil.java
@@ -0,0 +1,29 @@
+package thinkers.hmm.util;
+
+import java.util.ArrayList;
+
+/**
+ * Created by chaoli on 7/18/15.
+ */
+public class CourseFacultyRelationshipUtil {
+
+    public ArrayList<Integer> selectCourses(int fid){
+        return null;
+    }
+
+    public ArrayList<Integer> selectFaculties(int cid){
+        return null;
+    }
+
+    public boolean deleteByCourse(int cid){
+        return false;
+    }
+
+    public boolean deleteByFaculty(int fid){
+
+    }
+
+    public boolean insertRelationship(int cid, int fid){
+
+    }
+}
