7fd72433d0efdb2220987e57c61535ca3dad4625
Charlie <chaoli1@andrew.cmu.edu>
2015-07-18 20:07:38 -0400

    add construction

--- app/src/main/java/thinkers/hmm/model/FacultyReview.java
+++ app/src/main/java/thinkers/hmm/model/FacultyReview.java
@@ -1,5 +1,7 @@
 package thinkers.hmm.model;
 
+import java.util.Date;
+
 /**
  * Created by chaoli on 7/18/15.
  */
@@ -7,8 +9,9 @@ public class FacultyReview extends Review {
 
     private int fid;
 
-    public FacultyReview() {
-        super();
+    public FacultyReview(int id, int uid, int fid, String title, String content, String location, Date createdtime) {
+        super(id, uid, title, content, location, createdtime);
+        this.fid = fid;
     }
 
     public int getFid() {
