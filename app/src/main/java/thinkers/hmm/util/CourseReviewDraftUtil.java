a448034e711412d202920607c2061522e13a008f
LiGu <ligu92@gmail.com>
2015-07-24 18:32:50 -0400

    refactor names

---
+++ app/src/main/java/thinkers/hmm/util/CourseReviewDraftUtil.java
@@ -0,0 +1,19 @@
+package thinkers.hmm.util;
+
+/**
+ * Created by chaoli on 7/18/15.
+ */
+public class CourseReviewDraftUtil {
+
+    public CourseReviewDraftUtil selectDraft(int draftId){
+
+    }
+
+    public boolean updateDraft(int draftId, CourseReviewDraftUtil courseReviewDraft){
+
+    }
+
+    public boolean deleteDraft(int draftId){
+
+    }
+}
