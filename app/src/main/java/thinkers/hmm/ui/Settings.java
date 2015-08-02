8c40ccde1357419d6ad4622a49567e76a5b7fe12
DianeZhang <dianez1020@gmail.com>
2015-08-02 00:03:14 -0400

    added editing user settings

--- app/src/main/java/thinkers/hmm/ui/Settings.java
+++ app/src/main/java/thinkers/hmm/ui/Settings.java
@@ -20,6 +20,10 @@ import thinkers.hmm.util.AdminUtil;
 import thinkers.hmm.util.UserUtil;
 
 public class Settings extends Activity {
+    //Operation Strings
+    private final String GET_USER = "Get_User";
+    private final String GET_ADMIN = "Get_Admin";
+    private final String SAVE_SETTINGS = "Save_Settings";
 
     //MSG
     private final String ERROR_MSG = "Your Request Failed";
@@ -53,13 +57,20 @@ public class Settings extends Activity {
         // get role
         SharedPreferences sharedpreferences = getSharedPreferences(Login.USER_INFO, Context.MODE_PRIVATE);
         role = sharedpreferences.getString("role", null);
+        uid = sharedpreferences.getInt("uid", -1);
 
         //Set edit text field
         if(role.equals("user")) {
             //Get user
-            sharedpreferences = getSharedPreferences(Login.USER_INFO, Context.MODE_PRIVATE);
-            uid = sharedpreferences.getInt("uid", -1);
+            SettingHelper searchHelper = new SettingHelper();
+            String[] params= new String[1];
+            params[0] = GET_USER;
+            searchHelper.execute(params);
         } else {
+            SettingHelper searchHelper = new SettingHelper();
+            String[] params= new String[1];
+            params[0] = GET_ADMIN;
+            searchHelper.execute(params);
         }
     }
 
@@ -67,7 +78,9 @@ public class Settings extends Activity {
         @Override
         public void onClick(View v) {
             SettingHelper settingHelper = new SettingHelper();
-            settingHelper.execute();
+            String[] params= new String[1];
+            params[0] = SAVE_SETTINGS;
+            settingHelper.execute(params);
         }
     };
 
@@ -105,33 +118,56 @@ public class Settings extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    private class SettingHelper extends AsyncTask<Void, Void, Void> {
+    private class SettingHelper extends AsyncTask<Object, Void, Void> {
 
+        private String option = "";
+        private User user = null;
+        private Admin admin = null;
         private boolean operationResult = false;
-        @Override
-        protected Void doInBackground(Void... params) {
-            String username = editUserName.getText().toString();
-            String password = editPassword.getText().toString();
-            String email = editEmail.getText().toString();
 
-            if(role.equals("user")) {
+        @Override
+        protected Void doInBackground(Object... params) {
+            option = (String) params[0];
+            if (option.equals(GET_USER)) {
                 UserUtil userUtil = new UserUtil();
-                operationResult = userUtil.updateSetting(username, password, email, uid);
-            } else {
+                user = userUtil.selectUser(uid);
+            } else if (option.equals(GET_ADMIN)) {
                 AdminUtil adminUtil = new AdminUtil();
-                operationResult = adminUtil.updateSetting(username, password, email, uid);
+                admin = adminUtil.selectAdmin(uid);
+            } else if (option.equals(SAVE_SETTINGS)) {
+                String username = editUserName.getText().toString();
+                String password = editPassword.getText().toString();
+                String email = editEmail.getText().toString();
+
+                if (role.equals("user")) {
+                    UserUtil userUtil = new UserUtil();
+                    operationResult = userUtil.updateSetting(username, password, email, uid);
+                } else {
+                    AdminUtil adminUtil = new AdminUtil();
+                    operationResult = adminUtil.updateSetting(username, password, email, uid);
+                }
             }
             return null;
         }
 
         @Override
         protected void onPostExecute(Void object) {
-            if(operationResult == true) {
-                Toast.makeText(Settings.this, Success_Setting_Change_MSG, Toast.LENGTH_LONG)
-                .show();
-                finish();
-            } else {
-                Toast.makeText(Settings.this, ERROR_MSG, Toast.LENGTH_LONG).show();
+            if (option.equals(GET_USER)) {
+                editUserName.setText(user.getUsername());
+                editPassword.setText(user.getPassword());
+                editEmail.setText(user.getEmail());
+            } else if (option.equals(GET_ADMIN)) {
+                editUserName.setText(admin.getUsername());
+                editPassword.setText(admin.getPassword());
+                editEmail.setText(admin.getEmail());
+            } else if (option.equals(SAVE_SETTINGS)) {
+                if (operationResult == true) {
+                    Toast.makeText(Settings.this, Success_Setting_Change_MSG, Toast.LENGTH_LONG)
+                            .show();
+                    finish();
+                } else {
+                    Toast.makeText(Settings.this, ERROR_MSG, Toast.LENGTH_LONG).show();
+                }
             }
         }
     }
